813040ff675e37d4fbce30285d0b44b913a4029f
sk97kang <sk97.kang@gmail.com>
2020-07-13 17:53:12 +0900

    initialize

---
+++ WebContent/pages/home.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Travel Scan</title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+<body>
+<p>Home</p>
+</body>
+</html>
---
+++ src/controller/Controller.java
@@ -0,0 +1,44 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/travelscan/*")
+public class Controller extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		doProcess(req, res);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		doProcess(req, res);
+	}
+
+	protected void doProcess(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		String uri = req.getRequestURI();
+		// System.out.println(uri);
+		String action = uri.substring(uri.lastIndexOf("/") + 1);
+		if (action.equals("*")) {
+			action = "home.do";
+		}
+		String next = "";
+
+		if (action.equals("home.do")) {
+			next = "/pages/home.jsp";
+		}
+
+		if (!next.equals("")) {
+			RequestDispatcher dis = req.getRequestDispatcher(next);
+			dis.forward(req, res);
+		}
+	}
+
+}
